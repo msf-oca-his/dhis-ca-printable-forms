1be090c8f7ea485eb859c13ffb1fb075b438968c
srikanthkatta2207 <srikanthkatta2207@gmail.com>
2016-05-24 11:35:22 +0530

    #336[srikanth] added tests for render dataset in tallysheets controller

--- tests/controllers/TallySheetsCtrlSpec.js
+++ tests/controllers/TallySheetsCtrlSpec.js
@@ -6,7 +6,9 @@ describe("TallySheets ctrl", function () {
     var dataSetService;
     var mockedDataSetService;
     var httpMock;
-    var mockedProcessor;
+    var mockedDataSetProcessor;
+    var mockedProgramService;
+    var mockedProgramProcessor;
     beforeEach(function () {
         module("TallySheets");
         angular.module('d2HeaderBar', []);
@@ -15,6 +17,13 @@ describe("TallySheets ctrl", function () {
             id: "12",
             name: "general",
             sections: [{id: "1", name: "section1"}, {id: "2", name: "section2"}],
+            isResolved:Promise.resolve({})
+        };
+
+        var mockProgram={
+            id: "12",
+            name: "general",
+            stageSections: [{id: "1", name: "section1"}, {id: "2", name: "section2"}],
             isResolved:Promise.resolve({})
         };
 
@@ -36,17 +45,31 @@ describe("TallySheets ctrl", function () {
 
             }
         };
-        mockedProcessor = {
+        mockedProgramService = {
+            getProgram: function() {
+                defer.resolve(mockProgram);
+                return defer.promise;
+            }
+        };
+        mockedDataSetProcessor = {
             process: function() {
                 return expectedPages;
             }
         };
 
+        mockedProgramProcessor = {
+           process: function() {
+                return expectedPages;
+            }
+        };
 
         module(function ($provide) {
             $provide.value('OptionSetFactory', Promise.resolve({}));
             $provide.value('DataSetService',mockedDataSetService);
-            $provide.value('PrintFriendlyProcessor', mockedProcessor)
+            $provide.value('PrintFriendlyProcessor', mockedDataSetProcessor);
+            $provide.value('ProgramService', mockedProgramService);
+            $provide.value('ProgramProcessor',mockedProgramProcessor);
+
         });
     });
 
@@ -65,28 +88,62 @@ describe("TallySheets ctrl", function () {
         $controller('TallySheetsController', {$scope: scope});
     });
 
-    //describe("go home", function(){
-    //    it("should redirect to dhisurl", function(){
-    //       //scope.goHome();
-    //
-    //    });
-    //});
-
     describe("render datasets", function(){
         it("should not render dataset if it doesn't have form id", function(){
             scope.form.id = undefined;
             expect(scope.renderDataSets()).toEqual(Promise.resolve({}));
         });
 
-        it("should render the dataSets if it has form id", function(){
+        it("should render the dataSets if it has form id", function(done){
             scope.form.id = 123;
             scope.form.type ="dataset";
+            var expectedPages = [
+                {
+                    heightLeft: 188,
+                    width: 183,
+                    contents: [
+                        {type: 'dataSetName', name: "test dataset"},
+                        {type: 'section', section: {}}],
+                    datasetName: "test dataset"
+                }
+            ]
 
+            scope.renderDataSets().then(function(){
+                expect(scope.spinnerShown).toEqual(false);
+                expect(scope.pages).toEqual(expectedPages);
+                done();
+            });
+            setInterval(_$rootScope.$digest, 900);
+        });
+
+        it("should render the programs if it has form id", function(){
+            scope.form.id = 123;
+            scope.form.type ="program";
+            scope.programMode = true;
+            var expectedPages = [
+                {
+                    heightLeft: 188,
+                    width: 183,
+                    contents: [
+                        {type: 'dataSetName', name: "test program"},
+                        {type: 'section', section: {}}],
+                    datasetName: "test program"
+                }
+            ];
+
+            scope.renderDataSets().then(function(){
+                expect(scope.spinnerShown).toEqual(false);
+                expect(scope.pages).toEqual(expectedPages);
+                done();
+            });
+            setInterval(_$rootScope.$digest, 900);
+        });
+
+        it("should render the template which is neither program nor dataset if it has form id", function(){
+            scope.form.id = 123;
+            scope.form.type =""
             scope.renderDataSets();
-            //expect(scope.spinnerShown).toEqual("slkfj");
-            //expect(scope).toEqual("skfjs")
-
-            //_$rootScope.$apply();
-        })
+            expect(scope.spinnerShown).toEqual(false);
+        });
     })
 });
