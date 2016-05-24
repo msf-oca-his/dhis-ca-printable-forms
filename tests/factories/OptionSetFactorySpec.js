1f98551c3774e8fd5b65aec60593388fbb019f40
srikanthkatta2207 <srikanthkatta2207@gmail.com>
2016-05-24 14:30:10 +0530

    #336 srikanth added tests for optionset facotry

--- tests/factories/OptionSetFactorySpec.js
+++ tests/factories/OptionSetFactorySpec.js
@@ -1,24 +1,35 @@
 describe("Optionset Factory", function () {
     var httpMock;
     var optionSetFactory;
+    var rootScope;
     beforeEach(function () {
         module("TallySheets");
         angular.module('d2HeaderBar', []);
     });
 
-    beforeEach(inject(function($httpBackend,OptionSetFactory) {
+    beforeEach(inject(function($httpBackend,OptionSetFactory,$rootScope) {
+        rootScope = $rootScope;
         httpMock = $httpBackend;
         optionSetFactory = OptionSetFactory;
         httpMock.expectGET("languages/en.json").respond(200, {});
     }));
 
     describe('fecthing of optionsets', function(){
-        it("successful fecthing of optionsets", function(){
+        it("successful fecthing of optionsets", function(done){
             httpMock.expectGET("http://localhost:8000/api/optionSets.json?paging=false").respond(200, {
                 id: 123,
-                optionSets: [{id: 123}]
+                optionSets: [{id: 123, name:"optionset1"}]
             });
-            httpMock.expectGET("http://localhost:8000/api/optionSets/123.json?paging=false").respond(200, {})
+            httpMock.expectGET("http://localhost:8000/api/optionSets/123.json?paging=false").respond(200, {id: 123, name:"optionset1"});
+
+            var expectedOptionSets = {
+                123: {id:123,name:"optionset1"}
+            };
+            optionSetFactory.then(function(optionsets){
+                expect(optionsets).toEqual(expectedOptionSets);
+                done();
+            })
+            setInterval(rootScope.$digest,900);
             httpMock.flush();
         });
 
