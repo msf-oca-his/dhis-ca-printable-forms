d3073501eade15f53d85eb4b2e8bc3b5ed3ac4e9
Anil Kumar <aniltpl08@gmail.com>
2016-10-03 14:13:32 +0530

    [Anil/Sultan] #815 - changed translation provider initilization

---
+++ i18n/es.js
@@ -0,0 +1,12 @@
+module.exports ={
+  "HOME": "Inicio",
+	"PRINT": "Imprimir",
+	"EXPORT_EXCEL": "Exportar a MS Excel",
+	"SELECT_TEMPLATE": "Seleccione una plantilla",
+	"NO_ATTRIBUTE_EXISTS": "El UID especificado no existe en el sistema. Por favor, póngase en contacto con el administrador del sistema.",
+	"NO_ASSOCIATION_WITH_ENTITY": "No se encontró asociación entre el atributo y la entidad especificada en config",
+	"ATTRIBUTE_NOT_SET": "Atributo personalizado no se establece en true. Por favor, póngase en contacto con el administrador del sistema.",
+	"NO_ASSOCIATION_WITH_OPTIONSET":"El atributo especificado no está asociado con ningún optionset. Por favor, póngase en contacto con el administrador del sistema.",
+	"OPTIONSET_WITHOUT_OPTIONS": "El atributo específico de tipo optionset no tiene ninguna opción. Por favor, póngase en contacto con el administrador del sistema.",
+	"OPTIONSET_WITH_INCORRECT_OPTIONS": "Las opciones del atributo especificado son incorrectos. Por favor, póngase en contacto con el administrador del sistema."
+};
