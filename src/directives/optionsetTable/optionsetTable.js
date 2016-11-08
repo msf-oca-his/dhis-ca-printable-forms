3e802ced921351848de9f9699fb82bec391ce461
ArchanaChillala <archanac@thoughtworks.com>
2016-11-08 12:24:25 +0530

    [Archana] #413 - Deleted optionSetTable directive

--- src/directives/optionsetTable/optionsetTable.js
+++
@@ -1,9 +0,0 @@
-TallySheets.directive('optionsetTable', ['Config', function(config) {
-	return {
-		restrict: 'E',
-		template: require('./optionsetTableView.html'),
-		scope: {
-			dataelement: '='
-		}
-	}
-}]);
