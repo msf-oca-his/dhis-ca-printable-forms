1425bf26d4b8b46dc3cd67e285b93d5e7e29a383
sultanm <sultanahamer@gmail.com>
2016-12-06 19:29:00 +0530

    [Sultan] - Added tests for CatCombContent

--- tests/pff-model/CatCombSpec.js
+++ tests/pff-model/CatCombSpec.js
@@ -1,8 +1,20 @@
-xdescribe("addLineBreakAfterEachCategoryOption", function() {
-	var testSection;
+describe("CatCombContent", function() {
+	var testSection, CatCombContent;
 	beforeEach(function() {
+		module("TallySheets");
+		config = {
+			Delimiters: {
+				categoryOptionComboDelimiter: "<br>"
+			}
+		};
+		module(function($provide) {
+			$provide.value('Config', config);
+		});
+		inject(function(_CatCombContent_) {
+			CatCombContent = _CatCombContent_;
+		});
 		testSection = {
-			testKey: [{
+			dataElements: [{
 				categoryCombo: {
 					categoryOptionCombos: ["test,String1,", "test,String2,", "test, String3"]
 				}
@@ -10,21 +22,21 @@ xdescribe("addLineBreakAfterEachCategoryOption", function() {
 		};
 	});
 	it("should replace all , with <br>s", function() {
-		printFriendlyUtils.addLineBreakAfterEachCategoryOption(testSection, "testKey");
-		expect(testSection.testKey[0].categoryCombo.categoryOptionCombos[0]).toEqual("test<br>String1<br>");
+		var catCombContent = new CatCombContent(testSection)
+		expect(catCombContent.categoryOptionCombos[0]).toEqual("test<br>String1<br>");
 	});
 
 	it("should replace , with <br>s in all categoryOptionCombos", function() {
-		printFriendlyUtils.addLineBreakAfterEachCategoryOption(testSection, "testKey");
-		expect(testSection.testKey[0].categoryCombo.categoryOptionCombos[0]).toEqual("test<br>String1<br>");
-		expect(testSection.testKey[0].categoryCombo.categoryOptionCombos[1]).toEqual("test<br>String2<br>");
-		expect(testSection.testKey[0].categoryCombo.categoryOptionCombos[2]).toEqual("test<br> String3");
+		var catCombContent = new CatCombContent(testSection)
+		expect(catCombContent.categoryOptionCombos[0]).toEqual("test<br>String1<br>");
+		expect(catCombContent.categoryOptionCombos[1]).toEqual("test<br>String2<br>");
+		expect(catCombContent.categoryOptionCombos[2]).toEqual("test<br> String3");
 	});
 
 	it("should not replace any other character", function() {
 		var testString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()./>?:;'|\]}[{')\",";
-		testSection.testKey[0].categoryCombo.categoryOptionCombos.push(testString);
-		printFriendlyUtils.addLineBreakAfterEachCategoryOption(testSection, "testKey");
-		expect(testSection.testKey[0].categoryCombo.categoryOptionCombos[3]).toEqual(testString.replace(",", "<br>"));
+		testSection.dataElements[0].categoryCombo.categoryOptionCombos.push(testString);
+		var catCombContent = new CatCombContent(testSection)
+		expect(catCombContent.categoryOptionCombos[3]).toEqual(testString.replace(",", "<br>"));
 	})
 });
