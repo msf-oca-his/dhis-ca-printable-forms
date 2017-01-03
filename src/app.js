c03e55baf99c4cd49de2b75b7aca1519fb293eee
Anil Kumar <aniltpl08@gmail.com>
2017-01-03 12:25:58 +0530

    [Anil] model alert close functionality added

--- src/app.js
+++ src/app.js
@@ -125,6 +125,10 @@ TallySheets.controller('TallySheetsController', ['$scope', 'DataSetService', 'Da
 		window.location = dhisUrl;
 	};
 
+	$scope.closeModalAlert = function(context) {
+		window.location = dhisUrl;
+	};
+
 	$scope.renderTemplates = function(templates) {
 		$scope.pages = [];
 		$scope.templates = templates ? templates : $scope.templates;
