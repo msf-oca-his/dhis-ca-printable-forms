1b3f52f4f631b482527df2b89ceae822305bef3f
ArchanaChillala <archanac@thoughtworks.com>
2017-05-25 12:20:54 +0530

    [Archana] externalised value types in defaultContentView file

--- src/directives/content-directives/default-content/defaultContent.js
+++ src/directives/content-directives/default-content/defaultContent.js
@@ -1,9 +1,12 @@
-TallySheets.directive('defaultContent', [function() {
+TallySheets.directive('defaultContent', ['DhisConstants', function(DhisConstants) {
 	return {
 		restrict: 'E',
 		template: require('./defaultContentView.html'),
 		scope: {
 			content: '='
+		},
+		link: function($scope) {
+			$scope.booleanType = DhisConstants.ValueTypes.BOOLEAN;
 		}
 	}
 }]);
