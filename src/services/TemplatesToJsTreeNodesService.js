7fc6f6290ecdf3484ea69e1332fc23214d9091c2
Sultanahamer <sultanahamer@gmail.com>
2017-08-02 04:06:09 +0530

    [Sultan] - Added treenode types, index for each node and updated test cases.

--- src/services/TemplatesToJsTreeNodesService.js
+++ src/services/TemplatesToJsTreeNodesService.js
@@ -1,26 +1,35 @@
-TallySheets.service('TemplatesToJsTreeNodesService', ['DataSetAttributes', 'ProgramAttributes', function(DataSetAttributes, ProgramAttributes) {
+TallySheets.service('TemplatesToJsTreeNodesService', ['DataSetAttributes', 'ProgramAttributes', 'TreeNodeTypes', function(DataSetAttributes, ProgramAttributes, TreeNodeTypes) {
 
-	var node = function(id, text, index, childNodes) {
+	var node = function(id, text, index, type, childNodes) {
 		return {
 			'id': id,
 			'text': text,
 			'state': {'opened': true, 'selected': true},
 			'index': index,
-			'children': childNodes
+			'children': childNodes,
+			type: type
 		};
 	};
 
 	var getNodesFrom = function(template, attributes) {
 		var childNodes = _.map(_.get(template,attributes.SECTIONS), function(section, index) {
 			var childNodes = _.map(section[attributes.DATAELEMENTS], function(dataElement, index) {
-				return node(dataElement.id, dataElement.displayFormName, index, []);
+				return node(dataElement.id, dataElement.displayFormName, index, TreeNodeTypes.DATAELEMENT,[]);
 			});
-			return node(section.id, section.displayName, index, childNodes);
+			return node(section.id, section.displayName, index, TreeNodeTypes.SECTION, childNodes);
 		});
-		return node(template.id, template.displayName, attributes.TEMPLATE, childNodes);
+		return node(template.id, template.displayName, 0, TreeNodeTypes.TEMPLATE, childNodes);
 	};
 
 	this.getJsTreeNodes = function(template, templateType) {
 		return (templateType == "DATASET") ? getNodesFrom(template, new DataSetAttributes()) : getNodesFrom(template, new ProgramAttributes());
 	}
 }]);
+
+TallySheets.factory('TreeNodeTypes', function(){
+	return {
+			TEMPLATE: 'template',
+			SECTION: 'section',
+			DATAELEMENT: 'dataelement'
+	}
+});
--- tests/services/TemplatesToJsTreeNodesServiceSpec.js
+++ tests/services/TemplatesToJsTreeNodesServiceSpec.js
@@ -1,15 +1,16 @@
 describe('TemplatesToJsTreeNodesServiceSpec', function() {
 	var templatesToJsTreeNodesService;
 	var programAttributes, dataSetAttributes;
-	var dataSetTemplate, programTemplate;
+	var dataSetTemplate, programTemplate, treeNodeTypes;
 	beforeEach(function() {
 		angular.module('d2HeaderBar', []);
 		module("TallySheets");
 
-		inject(function($injector, TemplatesToJsTreeNodesService, DataSetAttributes, ProgramAttributes) {
+		inject(function($injector, TemplatesToJsTreeNodesService, DataSetAttributes, ProgramAttributes, TreeNodeTypes) {
 			templatesToJsTreeNodesService = TemplatesToJsTreeNodesService;
 			dataSetAttributes = DataSetAttributes;
 			programAttributes = ProgramAttributes;
+			treeNodeTypes = TreeNodeTypes;
 		});
 
 		dataSetTemplate = {
@@ -39,36 +40,52 @@ describe('TemplatesToJsTreeNodesServiceSpec', function() {
 
 	});
 
-	it("should get nodes from sections from templates of type dataset", function() {
+	it("should get nodes for dataset templates", function() {
 		var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(dataSetTemplate, 'DATASET');
 		expect('1').toEqual(actualNode.id);
 		expect('dataset1').toEqual(actualNode.text);
-		expect('template').toEqual(actualNode.path);
+		expect(0).toEqual(actualNode.index);
 		expect({'opened': true, 'selected': true}).toEqual(actualNode.state);
 		expect('s1').toEqual(actualNode.children[0].id);
 	});
 
-	it("should get correct path for inner dataelements and sections", function() {
+	it("should assign respective node type for each node in DATASET", function(){
+    dataSetTemplate.sections[1] = {dataElements: [{}, {}, {}]};
+    var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(dataSetTemplate, 'DATASET');
+    expect(treeNodeTypes.TEMPLATE).toEqual(actualNode.type);
+		expect(treeNodeTypes.SECTION).toEqual(actualNode.children[0].type);
+		expect(treeNodeTypes.DATAELEMENT).toEqual(actualNode.children[1].children[0].type);
+  });
+
+	it("should get correct index for inner dataelements and sections", function() {
 		dataSetTemplate.sections[1] = {dataElements: [{}, {}, {}]};
 		var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(dataSetTemplate, 'DATASET');
-		expect('template.sections[1]').toEqual(actualNode.children[1].path);
-		expect('template.sections[1].dataElements[1]').toEqual(actualNode.children[1].children[1].path);
+		expect(1).toEqual(actualNode.children[1].index);
+		expect(1).toEqual(actualNode.children[1].children[1].index);
 	});
 
-	it("should get nodes from templates of type of program", function() {
+	it("should get nodes for program templates", function() {
 		var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(programTemplate, 'PROGRAM');
 		expect('1').toEqual(actualNode.id);
 		expect('program1').toEqual(actualNode.text);
-		expect('template').toEqual(actualNode.path);
+		expect(0).toEqual(actualNode.index);
 		expect({'opened': true, 'selected': true}).toEqual(actualNode.state);
 		expect('2').toEqual(actualNode.children[0].id);
 	});
-	
-	it("should get correct path for inner dataelements and sections",function() {
+
+	it("should assign respective node type for each node in PROGEAM", function(){
+    programTemplate.programStages[0].programStageSections[1] = {programStageDataElements:[{},{},{}]};
+    var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(programTemplate, 'PROGRAM');
+    expect(treeNodeTypes.TEMPLATE).toEqual(actualNode.type);
+    expect(treeNodeTypes.SECTION).toEqual(actualNode.children[0].type);
+    expect(treeNodeTypes.DATAELEMENT).toEqual(actualNode.children[1].children[0].type);
+  });
+
+	it("should get correct path for inner dataelements and sections for programs",function() {
 		programTemplate.programStages[0].programStageSections[1] = {programStageDataElements:[{},{},{}]};
 		var actualNode = templatesToJsTreeNodesService.getJsTreeNodes(programTemplate,'PROGRAM');
-		expect('template.programStages[0].programStageSections[1]').toEqual(actualNode.children[1].path);
-		expect('template.programStages[0].programStageSections[1].programStageDataElements[1]').toEqual(actualNode.children[1].children[1].path)
+		expect(1).toEqual(actualNode.children[1].children[1].index);
+		expect(1).toEqual(actualNode.children[1].index);
 	})
 
 });
