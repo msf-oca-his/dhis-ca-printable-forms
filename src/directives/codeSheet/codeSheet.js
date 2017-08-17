4b343572d5932af0abd516cf121c6cca4a3c0b50
Sultanahamer <sultanahamer@gmail.com>
2017-08-17 13:47:57 +0530

    [Sultan] - Fixed codesheet printing

--- src/directives/codeSheet/codeSheet.js
+++ src/directives/codeSheet/codeSheet.js
@@ -8,7 +8,7 @@ TallySheets.directive('codeSheet', ['Config', 'CodeSheetElementTypes', function(
 		},
 		link: function($scope) {
 			$scope.rowHeight = config.CodeSheet.rowHeight + config.Metrics.mm;
-			var columnWidth = config.PageTypes.A4.Portrait.availableWidth / config.CodeSheet.numberOfColumns;
+			var columnWidth = config.PageTypes.A4.Portrait.availableWidth / config.CodeSheet.numberOfColumns - 1;
 			$scope.columnWidth = config.PageTypes.A4.Portrait.availableWidth / config.CodeSheet.numberOfColumns + config.Metrics.mm;
 			$scope.widthOfCode = config.CodeSheet.widthOfCode+ config.Metrics.mm;
 			$scope.widthOfOption = (columnWidth - config.CodeSheet.widthOfCode)+ config.Metrics.mm;
