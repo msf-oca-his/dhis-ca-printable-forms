72394f4d30fc76bc273ec4bed44d1958dab8e56f
srikantk <srikantk@INSrikantk.local>
2017-10-18 13:07:50 +0530

    [kaustav/srikanth] added api version in bootconfig.js

--- src/config/bootConfig.js
+++ src/config/bootConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
 	localAppUrl: "http://localhost:8000/",
-	apiVersion: ""   //leave this string blank if not required.
+	apiVersion: "26"   //leave this string blank if not required.
 };
