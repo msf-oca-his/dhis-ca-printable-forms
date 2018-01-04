27e3864a160288deb677d2cd28cfb327ac16d933
srikantk <srikantk@INSrikantk.local>
2018-01-04 12:29:50 +0530

    [Srikanth] added import-loader class in d2-ui-components library

--- src/d2-ui-components.js
+++ src/d2-ui-components.js
@@ -1,3 +1,3 @@
 window.d2Lib = require("../../dhis-ca-common/js/utils/d2-export.js");
-require("imports?angularModule=>TallySheets!../../dhis-ca-common/js/angular-commons/directives/d2-header-bar/d2HeaderBar.js");
-require("imports?angularModule=>TallySheets!../../dhis-ca-common/js/angular-commons/factories/D2Provider.js");
+require("imports-loader?angularModule=>TallySheets!../../dhis-ca-common/js/angular-commons/directives/d2-header-bar/d2HeaderBar.js");
+require("imports-loader?angularModule=>TallySheets!../../dhis-ca-common/js/angular-commons/factories/D2Provider.js");
