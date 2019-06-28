98120f13e37bb110129019aa2c0cc79aaa769483
Gaurav <gaurav.shahdeo@gmail.com>
2019-06-28 17:40:57 +0530

    [Gaurav, Swathi] #98 Fix Comments translation for registration tab

--- src/services/RegisterProcessor.js
+++ src/services/RegisterProcessor.js
@@ -1,70 +1,80 @@
-TallySheets.service('RegisterProcessor', ['RegisterPage', 'Content', 'RegisterColumn', 'ContentTypes', 'Config', 'PrintFriendlyUtils', function(RegisterPage, Content, RegisterColumn, ContentTypes, config, PrintFriendlyUtils) {
-	var page, currentPageIndex, pages;
+TallySheets.service('RegisterProcessor', ['RegisterPage', 'Content', 'RegisterColumn', 'ContentTypes', 'Config', 'PrintFriendlyUtils', 'CustomAngularTranslateService', function(RegisterPage, Content, RegisterColumn, ContentTypes, config, PrintFriendlyUtils, CustomAngularTranslateService) {
+	var page, currentPageIndex, pages, translatedCommentsValue;
 
 	this.process = function(program, pageConfig) {
-		var getNewPage = function() {
+        return getTranslatedUseCode()
+            .then(_.partial(processUtil, program, pageConfig));
+	};
+
+    var getTranslatedUseCode = function() {
+        return CustomAngularTranslateService.getTranslation("Comments");
+    };
+
+	var processUtil = function (program, pageConfig, translatedCode) {
+        translatedCommentsValue = translatedCode;
+        var getNewPage = function() {
             var availableHeight = pageConfig.height - (pageConfig.components.border.top + pageConfig.components.border.bottom);
             var availableWidth = pageConfig.width - (pageConfig.components.border.left + pageConfig.components.border.right);
-			page = new RegisterPage(availableHeight, availableWidth);
-			page.programName = program.displayName;
-			pages[++currentPageIndex] = page;
-			return page;
-		};
+            page = new RegisterPage(availableHeight, availableWidth);
+            page.programName = program.displayName;
+            pages[++currentPageIndex] = page;
+            return page;
+        };
 
-		var getColumnWidthOption = function(value) {
-			return _.filter(config.customAttributes.columnWidthOptionUID.columnWidthOptions, function(option) {
-				if(option.code == value) {
-					return {code: option.code, width: option.width}
-				}
-			})
-		};
-		var getDataElementRenderType = function(dataElement) {
-			if(config.customAttributes && config.customAttributes.columnWidthOptionUID){
-				var columnWidthAttribute = PrintFriendlyUtils.getCustomAttribute(dataElement.attributeValues, "columnWidthOptionUID");
-				if(columnWidthAttribute) {
-					return getColumnWidthOption(columnWidthAttribute.value)[0];
-				}
-			}
-			return {code:'', width: config.Register.defaultColumnWidth}
-		};
+        var getColumnWidthOption = function(value) {
+            return _.filter(config.customAttributes.columnWidthOptionUID.columnWidthOptions, function(option) {
+                if(option.code == value) {
+                    return {code: option.code, width: option.width}
+                }
+            })
+        };
+        var getDataElementRenderType = function(dataElement) {
+            if(config.customAttributes && config.customAttributes.columnWidthOptionUID){
+                var columnWidthAttribute = PrintFriendlyUtils.getCustomAttribute(dataElement.attributeValues, "columnWidthOptionUID");
+                if(columnWidthAttribute) {
+                    return getColumnWidthOption(columnWidthAttribute.value)[0];
+                }
+            }
+            return {code:'', width: config.Register.defaultColumnWidth}
+        };
 
-		var distributeDataElementsToPages = function(allDataElements) {
-			_.map(allDataElements, function(dataElement, index) {
-				var dataElementRenderType = getDataElementRenderType(dataElement);
-				var registerColumn = new RegisterColumn(dataElement.displayFormName, dataElementRenderType);
-				page.widthLeft = page.widthLeft - dataElementRenderType.width;
+        var distributeDataElementsToPages = function(allDataElements) {
+            _.map(allDataElements, function(dataElement, index) {
+                var dataElementRenderType = getDataElementRenderType(dataElement);
+                var registerColumn = new RegisterColumn(dataElement.displayFormName, dataElementRenderType);
+                page.widthLeft = page.widthLeft - dataElementRenderType.width;
 
-				if((allDataElements.length == (index + 1)) && page.widthLeft > 0) {
-					registerColumnsPerPage.push(registerColumn);
-				}
-				else if(((allDataElements.length - 1) == (index + 1)) && page.widthLeft > getDataElementRenderType(allDataElements[index + 1]).width) {
-					registerColumnsPerPage.push(registerColumn);
-				}
-				else if(((index + 1) < allDataElements.length - 1 ) && page.widthLeft > 0) {
-					registerColumnsPerPage.push(registerColumn);
-				}
-				else {
-					page.contents.push(new Content(ContentTypes.registerContent, registerColumnsPerPage));
-					registerColumnsPerPage = [];
-					page = getNewPage();
-					var registerColumnRenderType = getDataElementRenderType(dataElement);
-					page.widthLeft = page.widthLeft - registerColumnRenderType.width;
-					registerColumnsPerPage.push(new RegisterColumn(dataElement.displayFormName, registerColumnRenderType));
-				}
-			});
-		};
+                if((allDataElements.length == (index + 1)) && page.widthLeft > 0) {
+                    registerColumnsPerPage.push(registerColumn);
+                }
+                else if(((allDataElements.length - 1) == (index + 1)) && page.widthLeft > getDataElementRenderType(allDataElements[index + 1]).width) {
+                    registerColumnsPerPage.push(registerColumn);
+                }
+                else if(((index + 1) < allDataElements.length - 1 ) && page.widthLeft > 0) {
+                    registerColumnsPerPage.push(registerColumn);
+                }
+                else {
+                    page.contents.push(new Content(ContentTypes.registerContent, registerColumnsPerPage));
+                    registerColumnsPerPage = [];
+                    page = getNewPage();
+                    var registerColumnRenderType = getDataElementRenderType(dataElement);
+                    page.widthLeft = page.widthLeft - registerColumnRenderType.width;
+                    registerColumnsPerPage.push(new RegisterColumn(dataElement.displayFormName, registerColumnRenderType));
+                }
+            });
+        };
 
-		pages = [];
-		currentPageIndex = -1;
-		page = getNewPage();
-		var allDataElements = _.flatten(_.map(program.programStages[0].programStageSections, 'programStageDataElements'));
-		allDataElements = PrintFriendlyUtils.getDataElementsToDisplay(allDataElements);
-		var defaultRenderType = { code: '',width: config.Register.defaultColumnWidth };
-		var registerColumnsPerPage = [];
-		distributeDataElementsToPages(allDataElements);
-		registerColumnsPerPage.push(new RegisterColumn('Comments', defaultRenderType));
-		if(!_.isEmpty(registerColumnsPerPage))
-			page.contents.push(new Content(ContentTypes.registerContent, registerColumnsPerPage));
-		return pages;
-	};
+        pages = [];
+        currentPageIndex = -1;
+        page = getNewPage();
+        var allDataElements = _.flatten(_.map(program.programStages[0].programStageSections, 'programStageDataElements'));
+        allDataElements = PrintFriendlyUtils.getDataElementsToDisplay(allDataElements);
+        var defaultRenderType = { code: '',width: config.Register.defaultColumnWidth };
+        var registerColumnsPerPage = [];
+        distributeDataElementsToPages(allDataElements);
+        registerColumnsPerPage.push(new RegisterColumn(translatedCommentsValue, defaultRenderType));
+        if(!_.isEmpty(registerColumnsPerPage))
+            page.contents.push(new Content(ContentTypes.registerContent, registerColumnsPerPage));
+        return pages;
+    };
 }]);
